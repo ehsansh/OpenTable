6002103831fe277a21786e7939844756ef9d0d27
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-04-29 20:06:48 +0330

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,15 @@ export default function Home() {
                         </div>
                     </div>
                 </nav>
+                <main>
+                    <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2 '>
+                        <div className='text-center mt-10'>
+                            <h1 className='text-white text-5xl font-bold mt-2 mb-2'>
+                                Find your table for any occasion
+                            </h1>
+                        </div>
+                    </div>
+                </main>
             </main>
         </main>
     );
