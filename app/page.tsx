dfd51d9fd95206f9539eb8085e64dbfd94a8f1bd
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-04-30 13:46:16 +0330

    add card

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,32 @@ export default function Home() {
                             </button>
                         </div>
                     </div>
+                    <div className='py-3 px-36 mt-10 flex flex-wrap'>
+                        <div className='w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer'>
+                            <img
+                                src='https://resizer.otstatic.com/v2/photos/wide-medium/4/32602466.png'
+                                alt=''
+                                className='w-full h-36'
+                            />
+                            <div className='p-1'>
+                                <h3 className='font-bold text-2xl mb-2'>
+                                    Peter Pane - Kassel
+                                </h3>
+                                <div className='flex items-start'>
+                                    <div className='flex mb-2'>*****</div>
+                                    <p className='ml-2'>65 reviews</p>
+                                </div>
+                                <div className='flex text-reg font-light capitalize'>
+                                    <p className='mr-3 '>Mexican</p>
+                                    <p className='mr-3'>$$$</p>
+                                    <p>Toronto</p>
+                                </div>
+                                <p className='text-sm mt-1 font-bold'>
+                                    Booked 3 times today
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </main>
             </main>
         </main>
