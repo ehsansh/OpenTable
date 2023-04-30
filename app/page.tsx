6d3551c4875afafccfae3198b84850cbd44cc931
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-04-30 15:17:16 +0330

    menu title and description of restaurant page

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,41 @@ export default function Home() {
                         </h1>
                     </div>
                 </div>
+                <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11 '>
+                    <div className='bg-white w-[70%] rounded p-3 shadow'>
+                        <nav className='flex text-reg border-b pb-2'>
+                            <a href='' className='mr-7'>
+                                Overview
+                            </a>
+                            <a href='' className='mr-7'>
+                                Menu
+                            </a>
+                        </nav>
+                        <div className='mt-4 border-b pb-6'>
+                            <h1 className='font-bold text-6xl'>
+                                Peter Pane - Kassel{' '}
+                            </h1>
+                        </div>
+                        <div className='flex items-end'>
+                            <div className='ratings mt-2 flex items-center'>
+                                <p>*****</p>
+                                <p className='text-reg ml-3'>4.9</p>
+                            </div>
+                            <div>
+                                <p className='text-reg ml-4'>600 Reviews</p>
+                            </div>
+                        </div>
+                        <div className='mt-4'>
+                            <p className='text-lg font-light'>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Itaque et minus sint in,
+                                inventore eligendi placeat. Nulla, labore sit?
+                                Aut odit ratione optio doloremque ut dignissimos
+                                nisi? In, tenetur debitis?
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </main>
     );
