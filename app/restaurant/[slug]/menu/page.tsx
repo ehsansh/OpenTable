0c730b060e98efc47d971ea0bfedd0ec3f713ed4
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-05 16:08:09 +0330

    use layout for restaurant pages

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -6,12 +6,9 @@ import Menu from '../components/Menu';
 export default function RestarurantMenu() {
     return (
         <>
-            <Header />
-            <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-                <div className='bg-white w-[100%] rounded p-3 shadow'>
-                    <RestaurantNavbar />
-                    <Menu />
-                </div>
+            <div className='bg-white w-[100%] rounded p-3 shadow'>
+                <RestaurantNavbar />
+                <Menu />
             </div>
         </>
     );
