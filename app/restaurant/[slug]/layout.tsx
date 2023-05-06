5563f5309a2de29756dcea405d8951ade639ea4f
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-06 22:22:29 +0330

    add data to page component in restaurant/[slug] component

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -7,12 +7,14 @@ export const metadata = {
 
 export default function RestaurantLayout({
     children,
+    params,
 }: {
     children: React.ReactNode;
+    params: { slug: string };
 }) {
     return (
         <main>
-            <Header />
+            <Header name={params.slug} />
             <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
                 {children}
             </div>
