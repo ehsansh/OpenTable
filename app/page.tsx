5204559579350bd272332102ed7ad1d0d92348e0
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-06 15:51:11 +0330

    define interface for restaurant card type and fetch the data and pass them to RestaurantCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 
-export default function Home() {
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export interface RestaurantCardType {
+    id: number;
+    name: string;
+    main_image: string;
+    cuisine: Cuisine;
+    location: Location;
+    price: PRICE;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+    return await prisma.restaurant.findMany({
+        select: {
+            id: true,
+            name: true,
+            main_image: true,
+            cuisine: true,
+            location: true,
+            price: true,
+        },
+    });
+};
+
+export default async function Home() {
+    const restaurants = await fetchRestaurants();
+
     return (
         <main>
             <Header />
             <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-                <RestaurantCard />
+                {restaurants.map(restaurant => (
+                    <RestaurantCard restaurant={restaurant} />
+                ))}
             </div>
         </main>
     );
