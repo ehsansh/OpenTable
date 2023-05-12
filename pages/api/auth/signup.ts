909d1dc2ad3342ccded878ead7a972d0513787d3
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-12 12:11:22 +0330

    validate post req for sign up with validator

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import validator from 'validator';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') {
+        const { firstName, lastName, email, phone, city, password } = req.body;
+
+        const errors: string[] = [];
+
+        const validationSchema = [
+            {
+                valid: validator.isLength(firstName, {
+                    min: 1,
+                    max: 20,
+                }),
+                errorMessage: 'First name is invalid.',
+            },
+            {
+                valid: validator.isLength(lastName, {
+                    min: 1,
+                    max: 20,
+                }),
+                errorMessage: 'Last name is invalid.',
+            },
+            {
+                valid: validator.isEmail(email),
+                errorMessage: 'Email is invalid.',
+            },
+            {
+                valid: validator.isMobilePhone(phone),
+                errorMessage: 'Phone is invalid.',
+            },
+            {
+                valid: validator.isLength(city, {
+                    min: 1,
+                    max: 20,
+                }),
+                errorMessage: 'City is invalid.',
+            },
+            {
+                valid: validator.isStrongPassword(password),
+                errorMessage: 'Password is not strong.',
+            },
+        ];
+
+        validationSchema.map(check => {
+            if (!check.valid) {
+                errors.push(check.errorMessage);
+            }
+        });
+
+        if (errors.length) {
+            return res.status(400).json({ errorMessage: errors[0] });
+        }
+
+        res.status(200).json({
+            hello: 'body',
+        });
+    }
+}
