f621f3eb6bb1d8f8524f3b507238d629e361cd5d
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-14 11:50:44 +0330

    add time to restaurnat and filter it by opening window

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { useState } from 'react';
-import { partySize } from '../../../../data/index';
+import { partySize, times } from '../../../../data';
 
 import DatePicker from 'react-datepicker';
 
-export default function ReservationCard() {
+export default function ReservationCard({
+    openTime,
+    closeTime,
+}: {
+    openTime: string;
+    closeTime: string;
+}) {
     const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
     const handleChangeDate = (date: Date | null) => {
@@ -13,6 +19,26 @@ export default function ReservationCard() {
             return setSelectedDate(date);
         }
         return setSelectedDate(null);
+    };
+
+    const filterTimeByRestaurantOpenWindow = () => {
+        const timesWithinWindow: typeof times = [];
+
+        let isWithinWindow = false;
+
+        times.forEach(time => {
+            if (time.time === openTime) {
+                isWithinWindow = true;
+            }
+            if (isWithinWindow) {
+                timesWithinWindow.push(time);
+            }
+            if (time.time === closeTime) {
+                isWithinWindow = false;
+            }
+        });
+
+        return timesWithinWindow;
     };
 
     return (
@@ -42,8 +68,11 @@ export default function ReservationCard() {
                 <div className='flex flex-col w-[48%]'>
                     <label htmlFor=''>Time</label>
                     <select name='' id='' className='py-3 border-b font-light'>
-                        <option value=''>7:30 AM</option>
-                        <option value=''>9:30 AM</option>
+                        {filterTimeByRestaurantOpenWindow().map(time => (
+                            <option value={time.time}>
+                                {time.displayTime}
+                            </option>
+                        ))}
                     </select>
                 </div>
             </div>
