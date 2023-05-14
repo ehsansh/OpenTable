e812be45e11597706add312f27fbf80ab4609bac
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-14 11:26:31 +0330

    add datePicker to restaurantCard

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,8 +1,20 @@
 'use client';
 
+import { useState } from 'react';
 import { partySize } from '../../../../data/index';
 
+import DatePicker from 'react-datepicker';
+
 export default function ReservationCard() {
+    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+    const handleChangeDate = (date: Date | null) => {
+        if (date) {
+            return setSelectedDate(date);
+        }
+        return setSelectedDate(null);
+    };
+
     return (
         <div className='bg-white rounded p-3 shadow'>
             <div className='text-center border-b pb-2 font-bold'>
@@ -19,9 +31,12 @@ export default function ReservationCard() {
             <div className='flex justify-between'>
                 <div className='flex flex-col w-[48%]'>
                     <label htmlFor=''>Date</label>
-                    <input
-                        type='text'
-                        className='py-3 border-b font-light w-28'
+                    <DatePicker
+                        selected={selectedDate}
+                        onChange={handleChangeDate}
+                        className='py-3 border-b font-light text-reg w-24'
+                        dateFormat='MMMM d'
+                        wrapperClassName='w-[48%]'
                     />
                 </div>
                 <div className='flex flex-col w-[48%]'>
