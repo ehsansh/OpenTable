1efd533b93a8fc142ed3501e1394bf85f0a5fd5b
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-15 12:18:14 +0330

    write a query to get bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { times } from '../../../../data';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default async function handler(
     req: NextApiRequest,
@@ -29,5 +32,19 @@ export default async function handler(
         });
     }
 
-    return res.json({ slug, day, time, partySize });
+    const bookings = await prisma.booking.findMany({
+        where: {
+            booking_time: {
+                gte: new Date(`${day}T${searchTimes[0]}`),
+                lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+            },
+        },
+        select: {
+            number_of_people: true,
+            booking_time: true,
+            tables: true,
+        },
+    });
+
+    return res.json({ searchTimes, bookings });
 }
