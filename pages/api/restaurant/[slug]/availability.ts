bc4f76dcc6bc6e5d5c41fcf22eeab7acc5fde828
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-15 13:50:26 +0330

    compress booking

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -46,5 +46,17 @@ export default async function handler(
         },
     });
 
+    const bookingTablesObj: { [key: string]: { [key: number]: true } } = {};
+
+    bookings.forEach(booking => {
+        bookingTablesObj[booking.booking_time.toISOString()] =
+            booking.tables.reduce((obj, table) => {
+                return {
+                    ...obj,
+                    [table.table_id]: true,
+                };
+            }, {});
+    });
+
     return res.json({ searchTimes, bookings });
 }
