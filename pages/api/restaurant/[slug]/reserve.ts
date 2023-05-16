0e0b46ef26c73d597fc6e05dac9eb970b0030edc
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-05-16 11:37:13 +0330

    validate data that is sent for reserve endpoint

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,10 +1,38 @@
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     const { slug, day, time, partySize } = req.query as {
         slug: string;
         day: string;
         time: string;
         partySize: string;
     };
+
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug,
+        },
+    });
+
+    if (!restaurant) {
+        return res.status(404).json({
+            errorMessage: 'Invalid data provided',
+        });
+    }
+
+    if (
+        new Date(`${day}T${time}`) <
+            new Date(`${day}T${restaurant.open_time}`) ||
+        new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+    ) {
+        return res.status(404).json({
+            errorMessage: 'Restaurant is not open at that time',
+        });
+    }
 }
